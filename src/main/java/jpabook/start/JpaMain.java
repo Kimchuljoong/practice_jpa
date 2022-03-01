4744c1b2d7c65d9513ad78295d6ede4a9cae3230
kimchj <wnmh123@gmail.com>
2022-03-01 21:32:26 +0900

    practice1) EntityFactoryManager, EntityManager, EntityTransaction

--- src/main/java/jpabook/start/JpaMain.java
+++ src/main/java/jpabook/start/JpaMain.java
@@ -1,5 +1,56 @@
 package jpabook.start;
 
-public class JpaMain {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void logic(EntityManager em){
+
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUserName("kim");
+        member.setAge(3);
+
+        // 등록
+        em.persist(member);
+
+        // 수정
+        member.setAge(20);
+
+        // 한 건 조회
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember=" + findMember.getUserName() + ", age=" + findMember.getAge());
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        System.out.println("memberListSize=" + members.size());
+
+        // 삭제
+        em.remove(member);
+
+    }
 }
--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -7,18 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Data
-@Entity
-@Table(name="Member")
+@Data   // lombok getter, setter 자동으로 만들어줌 **너무 편함**
+@Entity // 클래스를 테이블과 매핑한다
+@Table(name="Member")   // 현재 클래스와 매핑되는 테이블의 명
 public class Member {
 
-    @Id
-    @Column(name = "ID")
+    @Id // 테이블의 기본키에 매핑
+    @Column(name = "ID")    // 해당 변수에 매핑되는 컬럼명
     private String id;
 
     @Column(name = "NAME")
     private String userName;
 
-    private Integer age;
+    private Integer age;    // 변수명과 동일한 컬럼명에 매핑
 
 }
