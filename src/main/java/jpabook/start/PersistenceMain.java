4a8ad305c41f44cf3b4450c7fddc51ed39fe181b
kimchj <wnmh123@gmail.com>
2022-03-02 00:56:24 +0900

    practice2) persistence context practice

---
+++ src/main/java/jpabook/start/PersistenceMain.java
@@ -0,0 +1,98 @@
+package jpabook.start;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class PersistenceMain {
+
+    public static void main(String[] args) {
+
+        testDetached();
+
+
+    }
+
+    public static void testDetached(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try {
+            transaction.begin();
+
+            Member member = new Member();
+            member.setId("memberA");
+            member.setUserName("회원A");
+
+            em.persist(member);
+
+            em.detach(member);
+
+            transaction.commit();
+        }catch (Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+    public static void testClear(){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try{
+
+            transaction.begin();
+
+            Member member = em.find(Member.class, "memberA");
+
+            em.clear();
+
+            member.setUserName("changeName");
+
+            transaction.commit();
+
+        }catch (Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+    public static void closeEntityManager() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        try{
+
+            transaction.begin();
+
+            Member memberA = em.find(Member.class, "memberA");
+            Member memberB = em.find(Member.class, "memberB");
+
+            transaction.commit();
+
+        }catch (Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+
+    }
+
+}
